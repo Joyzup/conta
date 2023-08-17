7afb67c80c69af5f6099e6670408d9d90888922a
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-08-17 17:59:20 -0300

    :rocket: API finalizada

--- src/main/java/com/factory/contabancaria/ContabancariaApplication.java
+++ src/main/java/com/factory/contabancaria/ContabancariaApplication.java
@@ -2,7 +2,7 @@ package com.factory.contabancaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Atualizações feitas
 @SpringBootApplication
 public class ContabancariaApplication {
 
