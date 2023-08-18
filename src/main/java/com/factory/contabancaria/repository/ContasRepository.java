7633aa51ba0b312a719dc1163b0c299e30d7d55b
elizabetholiveira <elizabeth.oliveira@zup.com.br>
2023-08-18 09:25:35 -0300

    segundo commit beth

--- src/main/java/com/factory/contabancaria/repository/ContasRepository.java
+++ src/main/java/com/factory/contabancaria/repository/ContasRepository.java
@@ -3,6 +3,7 @@ package com.factory.contabancaria.repository;
 import com.factory.contabancaria.model.ContasModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +11,6 @@ import java.util.Optional;
 @Repository
 public interface ContasRepository extends JpaRepository<ContasModel, Long> {
 
-    @Query(value = "SELECT * FROM TB_CONTAS WHERE nomeDoUsuario = :nome")
-    Optional<ContasModel> findByNome(String nome);
+    @Query(value = "SELECT * FROM TB_CONTAS WHERE nomeDoUsuario = :nome", nativeQuery = true)
+    Optional<ContasModel> findByNome(@Param("nome") String nome);
 }
